41a817e11efed756ddde1443c8708b8b1bd9dddf
Yiyang Tan <140795018+Tanngent2005@users.noreply.github.com>
2024-02-29 23:46:35 +0800

    feat: implemented login endpoint

--- backend/main.go
+++ backend/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 
@@ -9,6 +10,15 @@ import (
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
+
+type LoginResponse struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+	Data    struct {
+		Type string `json:"type"`
+		ID   string `json:"id"`
+	} `json:"data"`
+}
 
 // 定义基本的 User 模型
 type User struct {
@@ -37,24 +47,67 @@ func main() {
 	db.AutoMigrate(&User{})
 
 	// 设置路由
+	r.Use(corsMiddleware())
+	r.POST("/your-endpoint", YourHandler)
+
+	// 运行服务
 
 	// Hello World HTTP Endpoint -> JSON
-	r.GET("/helloworld", func(c *gin.Context) {
+	r.POST("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "Hello, World!",
 		})
 	})
 
-	// Database Endpoint -> JSON
-	r.GET("/users", func(c *gin.Context) {
-		var users []User
-		db.Find(&users)
-		c.JSON(http.StatusOK, users)
-	})
+	r.POST("/api/v1/login", func(c *gin.Context) {
+		response := LoginResponse{
+			Code:    200,
+			Message: "登录成功",
+		}
+		response.Data.Type = "Student"
+		response.Data.ID = "123456"
+		var body = make([]byte, 0)
+		c.Request.Body.Read(body)
+		fmt.Print(body)
 
+		c.JSON(http.StatusOK, response)
+	})
 	// 运行服务
-	err = r.Run(":8080")
+	err = r.Run(":18080")
 	if err != nil {
 		log.Fatal(err)
 	}
 }
+
+func corsMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusNoContent)
+			return
+		}
+		c.Next()
+	}
+}
+
+func YourHandler(c *gin.Context) {
+	// 读取请求体中的数据
+	//body,
+	body, err := ioutil.ReadAll(c.Request.Body)
+
+	if err != nil {
+		// 处理读取请求体错误
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to read request body"})
+		return
+	}
+
+	// 打印请求体中的数据
+	fmt.Println("string(body)")
+	fmt.Println(string(body))
+	// 其他处理逻辑...
+}
+
+//http://127.0.0.1:18080/api/v1/login环境
