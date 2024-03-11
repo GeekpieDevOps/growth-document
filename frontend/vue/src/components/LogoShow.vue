f78e56facb98aca8fb6dbc6395f1504ba30aa69e
Lizijian <3161584612@qq.com>
2024-03-11 14:40:17 +0800

    build: change the imgs' src

--- frontend/vue/src/components/LogoShow.vue
+++ frontend/vue/src/components/LogoShow.vue
@@ -2,19 +2,19 @@
 import { ref } from "vue";
 const datas = ref([
   {
-    imgURL: "/src/assets/上道书院.png",
+    imgURL: "/imgs/上道书院.png",
     webURL: "https://shangdao.shanghaitech.edu.cn/",
     imgColor: "#0527af",
     name: "上道书院",
   },
   {
-    imgURL: "/src/assets/科道书院.png",
+    imgURL: "/imgs/科道书院.png",
     webURL: "https://kedao.shanghaitech.edu.cn/",
     imgColor: "#b188bb",
     name: "科道书院",
   },
   {
-    imgURL: "/src/assets/大道书院.png",
+    imgURL: "/imgs/大道书院.png",
     webURL: "https://dadao.shanghaitech.edu.cn/",
     imgColor: "#688a51",
     name: "大道书院",
