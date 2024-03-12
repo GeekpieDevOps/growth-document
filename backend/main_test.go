04047b340a20867fe08e36f1e7d951ba0eae9933
Sizhe Zhao <prc.zhao@outlook.com>
2024-03-12 15:17:38 +0800

    test(backend): add test

---
+++ backend/main_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// TODO: split into small sections for different endpoints
+// TODO: use temporary database
+func TestEverything(t *testing.T) {
+	db, err := setupPostgres()
+	assert.NoError(t, err)
+
+	router := setupRouter(db)
+
+	t.Run("POST /api/v1/register 500", func(t *testing.T) {
+		req, err := http.NewRequest(
+			"POST", "/api/v1/register",
+			strings.NewReader(``))
+		assert.NoError(t, err)
+
+		w := httptest.NewRecorder()
+		router.ServeHTTP(w, req)
+		assert.Equal(t, 500, w.Code)
+	})
+}
