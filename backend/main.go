8e1b5978f36303298e24827f172df4f6f1a1997a
Sizhe Zhao <prc.zhao@outlook.com>
2024-03-12 09:06:52 +0800

    style(backend): remove redundant comments

--- backend/main.go
+++ backend/main.go
@@ -44,7 +44,6 @@ type User struct {
 func UpdateUser(c *gin.Context, db *gorm.DB) {
 	id := c.Param("id")
 
-	//检查用户是否存在
 	var user User
 	result := db.First(&user, id)
 	if result.Error != nil {
@@ -52,7 +51,6 @@ func UpdateUser(c *gin.Context, db *gorm.DB) {
 		return
 	}
 
-	//对用户的email, password, nickname进行更新
 	var requestData struct {
 		Email    string `json:"email"`
 		Password string `json:"password"`
@@ -84,7 +82,6 @@ func UpdateUser(c *gin.Context, db *gorm.DB) {
 func DeleteUser(c *gin.Context, db *gorm.DB) {
 	id := c.Param("id")
 
-	// 检查用户是否存在
 	var user User
 	result := db.First(&user, id)
 	if result.Error != nil {
@@ -92,7 +89,6 @@ func DeleteUser(c *gin.Context, db *gorm.DB) {
 		return
 	}
 
-	// 删除用户
 	result = db.Delete(&user)
 	if result.Error != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"code": 500, "message": "Failed to delete user"})
@@ -131,8 +127,6 @@ func setupRouter(db *gorm.DB) *gin.Engine {
 }
 
 func main() {
-	// 初始化 Gorm
-	// var db *gorm.DB // 由于后面的代码中使用的是简短模式 := ，此处的定义是冗余的
 	// 临时数据库启动命令(Docker): docker run -id --name=postgres-test -v postgre-data:/var/lib/postgresql/data -p 5432:5432 -e POSTGRES_PASSWORD=123456 -e LANG=C.UTF-8 postgres
 	dsn := "host=localhost user=postgres password=yourpassword dbname=postgres port=5432 sslmode=disable TimeZone=Asia/Shanghai" //postgres 是 PostgreSQL 的默认用户名和数据库名称
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})                                                                     //"   "应该填链接，由于我不知道postgres的链接形式，所以这里只是一个示例
@@ -180,5 +174,3 @@ func corsMiddleware() gin.HandlerFunc {
 	fmt.Println(body)
 	// 其他处理逻辑...
 }写的貌似有问题，暂时弃用*/
-
-//http://127.0.0.1:18080/api/v1/login
