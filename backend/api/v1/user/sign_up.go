a2765c617cab782d2657426b2f526cd52c9231fb
Sizhe Zhao <prc.zhao@outlook.com>
2024-03-13 12:33:14 +0800

    docs(backend/api/v1/user): document SignUp

--- backend/api/v1/user/sign_up.go
+++ backend/api/v1/user/sign_up.go
@@ -24,10 +24,14 @@ type SignUpResponse struct {
 	UUID uuid.UUID `json:"uuid" binding:"required,uuid"`
 }
 
+// SignIn is the handler for /api/v1/user/sign_up.
 func SignUp(db *gorm.DB) func(c *gin.Context) {
 	return func(c *gin.Context) {
 		var req SignUpRequest
+
+		// Parse request payload
 		if err := c.ShouldBindJSON(&req); err != nil {
+			// Inform the client about invalid fields
 			if v, ok := err.(validator.ValidationErrors); ok {
 				c.AbortWithStatusJSON(http.StatusBadRequest,
 					lo.Map(v, func(f validator.FieldError, _ int) string {
@@ -37,20 +41,26 @@ func SignUp(db *gorm.DB) func(c *gin.Context) {
 				return
 			}
 
+			// Can't handle this, so abort
 			c.AbortWithStatus(http.StatusBadRequest)
 			return
 		}
 
 		var user models.User
+
+		// Check if the user has already signed up
 		result := db.Where("email = ?", req.Email).First(&user)
 		if result.Error == nil {
+			// User already exists
 			c.AbortWithStatus(http.StatusConflict)
 			return
 		} else if !errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			// Can't handle this, so abort
 			c.AbortWithError(http.StatusInternalServerError, result.Error)
 			return
 		}
 
+		// Generate nonce with CSRNG
 		nonce := make([]byte, 16)
 		_, err := rand.Read(nonce)
 		if err != nil {
