e75d1c643d369ddfc7eb5c3924df083245a0092f
Sizhe Zhao <prc.zhao@outlook.com>
2024-03-13 11:38:31 +0800

    docs(backend/api/v1/user): document Mount

--- backend/api/v1/user/user.go
+++ backend/api/v1/user/user.go
@@ -6,6 +6,8 @@ import (
 	"gorm.io/gorm"
 )
 
+// Mount mounts handlers under r.
+// It also configures them to use db for database access.
 func Mount(r *gin.RouterGroup, db *gorm.DB) {
 	r.POST("/sign_up", SignUp(db))
 	r.POST("/sign_in", SignIn(db))
