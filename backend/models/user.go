043f731fa13cf2d6a0564208aeb6c6a6c63bc27c
Sizhe Zhao <prc.zhao@outlook.com>
2024-03-13 11:21:03 +0800

    docs(backend/models): document User

--- backend/models/user.go
+++ backend/models/user.go
@@ -4,11 +4,25 @@ import (
 	"github.com/google/uuid"
 )
 
+// User contains user information.
 type User struct {
-	UUID     uuid.UUID `gorm:"primaryKey"`
-	ID       string
-	Email    string
+	// UUID is the unique identifier of the user.
+	UUID uuid.UUID `gorm:"primaryKey"`
+
+	// ID is the ID of the user. One example is the student ID.
+	ID string
+
+	// Email is the email address of the user.
+	Email string
+
+	// Password is store as plain text.
+	//
+	// TODO: store password hash and salt.
 	Password string
+
+	// Nickname contains the display name.
 	Nickname string
-	Nonce    []byte
+
+	// Nonce is a per-user secret for signing JWT.
+	Nonce []byte
 }
