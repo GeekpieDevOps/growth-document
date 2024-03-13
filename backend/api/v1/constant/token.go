6dc5c30e03e0dbbfb614ab0eca8ba0df590104fe
Sizhe Zhao <prc.zhao@outlook.com>
2024-03-13 11:34:43 +0800

    docs(backend/api/v1/constant): document JWTSubjectAuthentication

--- backend/api/v1/constant/token.go
+++ backend/api/v1/constant/token.go
@@ -3,5 +3,6 @@ package constant
 import "github.com/google/uuid"
 
 var (
+	// JWTSubjectAuthentication is the JWT subject for authentication.
 	JWTSubjectAuthentication = uuid.MustParse("c3ac10d7-6526-4684-86a8-7074a3e0bd38")
 )
