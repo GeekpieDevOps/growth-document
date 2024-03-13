62fb9d93af9fb46cb3777882015d2eeecb9e92c3
Sizhe Zhao <prc.zhao@outlook.com>
2024-03-13 11:41:15 +0800

    docs(backend/api/v1/user): document SignInRequest

--- backend/api/v1/user/sign_in.go
+++ backend/api/v1/user/sign_in.go
@@ -14,6 +14,7 @@ import (
 	"gorm.io/gorm"
 )
 
+// SignInRequest contains fields for an sign in request.
 type SignInRequest struct {
 	Email    string `json:"email" binding:"required,email"`
 	Password string `json:"password" binding:"required"`
