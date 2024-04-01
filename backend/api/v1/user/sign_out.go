9860b6ad2cc765392763e2932c8d6873bbc0fe74
kuangpenghao <2653292820@qq.com>
2024-04-01 20:59:08 +0800

    upload sign_out.go

--- backend/api/v1/user/sign_out.go
+++ backend/api/v1/user/sign_out.go
@@ -4,12 +4,9 @@ import (
 	"errors"
 	"net/http"
 
-	"github.com/GeekpieDevOps/growth-document/backend/api/v1/constant"
 	"github.com/GeekpieDevOps/growth-document/backend/models"
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
-	"github.com/golang-jwt/jwt/v5"
-	"github.com/google/uuid"
 	"github.com/samber/lo"
 	"gorm.io/gorm"
 )
@@ -39,7 +36,7 @@ func SignOut(db *gorm.DB) func(c *gin.Context){
 		var token models.Token
 		result:=db.Where("token = ? AND uuid = ?",req.Token,req.UUID).First(&token)
 		if result.Error!=nil{
-			if errors.Is(result.Error,gorm.ErrRecoredNotFound){
+			if errors.Is(result.Error,gorm.ErrRecordNotFound){
 				//未找到用户的登录令牌
 				c.AbortWithStatus(http.StatusUnauthorized)
 				return
@@ -51,13 +48,13 @@ func SignOut(db *gorm.DB) func(c *gin.Context){
 		}
 
 		//删除相应的令牌，实现登出操作
-		result:=db.Where("token = ? AND uuid = ?",req.Token,req.UUID).Delete(&token)
-		if result.Error!=nil{
+		resultDelete:=db.Where("token = ? AND uuid = ?",req.Token,req.UUID).Delete(&token)
+		if resultDelete.Error!=nil{
 			//删除操作出错
-			c.AbortWithError(http.StatusInternalServerError,result.Error)
+			c.AbortWithError(http.StatusInternalServerError,resultDelete.Error)
 			return
 		}
-		if result.RowsAffected==0{
+		if resultDelete.RowsAffected==0{
 			//操作后数据库行数未收到影响，即未找到匹配的记录
 			c.AbortWithStatus(http.StatusNotFound)
 			return
